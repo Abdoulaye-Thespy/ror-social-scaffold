f1dcee14c5bfd4ddeabfc82fbf90aa838e567cd5
Abdoulaye-Thespy <njigouhrazak@iut-dhaka.edu>
2020-07-27 13:06:48 +0100

    made changes required by the TSE

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,10 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
+
+  def confirm_friend(user)
+    friendships = inverse_friendships.find { |friendship| friendship.user == user }
+    friendships.confirmed = true
+    friendships.save
+  end
 end
